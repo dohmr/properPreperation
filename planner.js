35b892a1c6923caee86de6a4d6ab1e3e5970d9a9
doh <theshawns0n@gmail.com>
2020-09-18 16:57:01 -0700

    able to render LOGS! planner color working

--- planner.js
+++ planner.js
@@ -2,16 +2,17 @@ var hi = "...doing great, buhd!";
 console.log(hi);
 // moment().toDate();
 // get Time and display in jumbo?
+var time = moment().hour();
 var date = moment().format("MMMM Do YYYY");
 console.log(date);
 var currentHour = moment().format("H");
 console.log(currentHour);
-
-var eachHour = [ $("#9"), "10", "11", "12", "13", "14", "15", "16", "17"]
+console.log(time);
+var eachHour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 // $(".description").text("Coffee???");
 $("#currentDay").append(date);
 // $(".lead").append(currentTime);
-console.log(eachHour);
+
 // save to local all input on each given hour
 // ????????????????save button on click to save .description input 
 // ???????????class = .description is our text input?????????
@@ -24,40 +25,43 @@ saveBtn.on("click", function () {
     console.log(timeOfday, textContent);
 });
 
-// function renderPlans() {
-//     var timeOfday = $(this).parent().attr("id");
-//     var textContent = localStorage.getItem(timeOfday, textContent);
+function renderPlans() {
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var description = localStorage.getItem(id);
 
-    
-//     console.log(timeOfday, textContent);
-// }
-// saveBtn.on("click", function () {
-//     var hour = $(this).parent().attr("id");
-//     var task = $(this).siblings(".description").val();
-
-//     localStorage.setItem(hour, task);
-//     console.log(hour, task);
-// });
-
+        if (description !== null) {
+            $(this).children(".description").val(description);
+        }
+    });
+}
+renderPlans();
 // render to html 
 // take from local storage and display/render  .getItem("hour-9")
 
 // show what has past present and what has not 
 // ??????????make times of day variable numbers associated with their hour ?   eachHour
 // ??????????take time of day ... change attribute to css style  past/present/future
-// ??????????take current time > currentHour
+// ??????????take current time > currentHour / time?
 
 
-function highlights() {
-    for (var i = 0; i < eachHour.length; i++) {
-        if (eachHour[i] > currentHour) {
+function changeColors() {
+    hour = time;
+    console.log(hour);
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+        console.log(thisHour);
+        if (thisHour > hour) {
             $(this).addClass("future")
         }
-        else if (eachHour[i] === currentHour) {
+        else if (thisHour === hour) {
             $(this).addClass("present");
         }
         else {
             $(this).addClass("past");
         }
-    }
+        
+    })
 }
+
+changeColors();
