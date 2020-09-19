8c407e54d821b2a1614c433b06fce2230849e5ab
doh <theshawns0n@gmail.com>
2020-09-18 17:23:28 -0700

    added notation

--- planner.js
+++ planner.js
@@ -1,56 +1,25 @@
 var hi = "...doing great, buhd!";
 console.log(hi);
-// moment().toDate();
-// get Time and display in jumbo?
+// time variables using moment.js
 var time = moment().hour();
 var date = moment().format("MMMM Do YYYY");
-console.log(date);
 var currentHour = moment().format("H");
-console.log(currentHour);
-console.log(time);
 var eachHour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-// $(".description").text("Coffee???");
+
 $("#currentDay").append(date);
-// $(".lead").append(currentTime);
-
-// save to local all input on each given hour
-// ????????????????save button on click to save .description input 
-// ???????????class = .description is our text input?????????
-var saveBtn = $(".saveBtn");
-saveBtn.on("click", function () {
-    var timeOfday = $(this).parent().attr("id");
-    var textContent = $(this).siblings(".description").val();
-
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
-});
-
-function renderPlans() {
-    $(".time-block").each(function () {
-        var id = $(this).attr("id");
-        var description = localStorage.getItem(id);
-
-        if (description !== null) {
-            $(this).children(".description").val(description);
-        }
-    });
-}
-renderPlans();
-// render to html 
-// take from local storage and display/render  .getItem("hour-9")
 
 // show what has past present and what has not 
-// ??????????make times of day variable numbers associated with their hour ?   eachHour
 // ??????????take time of day ... change attribute to css style  past/present/future
 // ??????????take current time > currentHour / time?
-
-
 function changeColors() {
     hour = time;
+    // hour = current time i.e. 11
     console.log(hour);
     $(".time-block").each(function () {
+        // run each function on every instance of time-block 
         var thisHour = parseInt($(this).attr("id"));
         console.log(thisHour);
+        // thisHour = time-blocks corresponding id# i.e. 9
         if (thisHour > hour) {
             $(this).addClass("future")
         }
@@ -60,8 +29,38 @@ function changeColors() {
         else {
             $(this).addClass("past");
         }
-        
+
     })
 }
-
 changeColors();
+// save to local: all input on each given hour
+// save button on click to save .description input 
+// class = .description is our text input
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function () {
+    var timeOfday = $(this).parent().attr("id");
+    // time of day is = the parent elements ( save btn ) id #
+    var textContent = $(this).siblings(".description").val();
+    // content is the the sibling of the save saveBtn, our text area 
+
+    localStorage.setItem(timeOfday, textContent);
+    console.log(timeOfday, textContent);
+});
+
+
+
+// render to html 
+// take from local storage and display/render  .getItem
+function renderPlans() {
+    $(".time-block").each(function () {
+        var divEl = $(this).attr("id");
+        // same routine as we did to save the items
+        var textContent = localStorage.getItem(divEl);
+
+        if (textContent !== null) {
+            $(this).children(".description").val(textContent);
+        }
+    });
+}
+
+renderPlans();
